144f2bb8c4d0ccfd047e9d1c6f9be538447311ac
Nathaniel Landau <nate@natelandau.com>
2022-01-17 21:02:49 -0500

    Fix stopwords for new file commits
    The methodology of writing diff changes for testing does not work
    when a new file is committed to git.  Added an if/then that checks an
    entire file against the stopwords files when it is initially added.

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -25,7 +25,7 @@ _mainScript_() {
         [[ $# == 0 ]] && fatal "Missing required argument to ${FUNCNAME[0]}"
 
         local _gitDiffTmp
-        _gitDiffTmp="${TMP_DIR}/diff.txt"
+        _gitDiffTmp="${TMP_DIR}/${RANDOM}.${RANDOM}.${RANDOM}.diff.txt"
 
         if [ -f "${STOP_WORD_FILE}" ]; then
 
@@ -38,15 +38,25 @@ _mainScript_() {
             # remove blank lines from stopwords file
             sed '/^$/d' "${STOP_WORD_FILE}" >"${TMP_DIR}/pattern_file.txt"
 
-            # Add diff to a temporary file
-            git diff --cached -- "${1}" | grep '^+' >"${_gitDiffTmp}"
-
-            if grep --file="${TMP_DIR}/pattern_file.txt" "${_gitDiffTmp}"; then
-                return 1
+            # Check for stopwords
+            if git diff --cached -- "${1}" | grep -q "new file mode"; then
+                if grep -q --file="${TMP_DIR}/pattern_file.txt" "${1}"; then
+                    return 1
+                else
+                    return 0
+                fi
             else
-                return 0
+                # Add diff to a temporary file
+                git diff --cached -- "${1}" | grep '^+' >"${_gitDiffTmp}"
+                if grep --file="${TMP_DIR}/pattern_file.txt" "${_gitDiffTmp}"; then
+                    return 1
+                else
+                    return 0
+                fi
             fi
+
         else
+
             notice "Could not find git stopwords file expected at '${STOP_WORD_FILE}'. Continuing..."
             return 0
         fi
